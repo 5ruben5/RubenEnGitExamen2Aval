1b836fe84665e9fb38120623e30215d0bb11b2b7
Rubén Martínez <rumama02@floridauniversitaria.es>
2023-02-16 10:19:06 +0100

    Subiendo el archivo bminymax.sh

--- bminymax.sh
+++ bminymax.sh
@@ -2,8 +2,8 @@
 
 # Obtener la línea 16 y 15 del archivo consumo.txt
 
-linea15=$(sed -n '15p' consumos.txt)
+linea8=$(sed -n '8p' consumos.txt)
 linea16=$(sed -n '16p' consumos.txt)
 
-echo "Este es el consumo minimo: $linea15"
+echo "Este es el consumo minimo: $linea8"
 echo "Este es el consumo maxiom: $linea16"
---
+++ emenor.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+menor=9999
+ciudad=""
+
+while read linea
+do
+  ciudad_actual=`echo $linea | awk '{print $1}'`
+  consumo_actual=`echo $linea | awk '{print $4}'`
+  if [ $consumo_actual -lt $menor ]
+  then
+    menor=$consumo_actual
+    ciudad=$ciudad_actual
+  fi
+done < consumos.txt
+
+echo "La ciudad con menor consumo es $ciudad con un consumo de $menor"
